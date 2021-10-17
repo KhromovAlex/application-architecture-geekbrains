b8be0351dedb654346a833ef1444c30e7fa11774
Alexandr Khromov <4-09@bk.ru>
2021-10-17 15:19:07 +0700

    added screen details

--- app/src/main/java/com/example/applicationarchitecturegeekbrains/presentation/movie/MovieDetailsFragment.kt
+++ app/src/main/java/com/example/applicationarchitecturegeekbrains/presentation/movie/MovieDetailsFragment.kt
@@ -1,14 +1,21 @@
 package com.example.applicationarchitecturegeekbrains.presentation.movie
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.applicationarchitecturegeekbrains.R
 import com.example.applicationarchitecturegeekbrains.presentation.abs.AbsFragment
+import com.example.applicationarchitecturegeekbrains.presentation.model.MovieModel
 import moxy.ktx.moxyPresenter
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
-class MovieDetailsFragment : AbsFragment(R.layout.fragment_movie_details) {
+class MovieDetailsFragment : AbsFragment(R.layout.fragment_movie_details), MovieDetailsView {
     @Inject
     lateinit var movieDetailsPresenterFactory: MovieDetailsPresenterFactory
 
@@ -18,11 +25,17 @@ class MovieDetailsFragment : AbsFragment(R.layout.fragment_movie_details) {
 
     private var id: Int? = null
 
+    private val poster: ImageView by lazy { requireActivity().findViewById(R.id.poster) }
+    private val title: TextView by lazy { requireActivity().findViewById(R.id.title) }
+    private val overview: TextView by lazy { requireActivity().findViewById(R.id.overview) }
+    private val date: TextView by lazy { requireActivity().findViewById(R.id.date) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         arguments?.let {
             id = it.getInt(ARG_ID)
         }
+        super.onCreate(savedInstanceState)
+
     }
 
     companion object {
@@ -34,5 +47,20 @@ class MovieDetailsFragment : AbsFragment(R.layout.fragment_movie_details) {
                     ARG_ID to id
                 )
             }
+    }
+
+    override fun showMovie(movie: MovieModel) {
+        title.text = movie.originalTitle
+        overview.text = movie.overview
+        date.text = if (movie.releaseDate != null) SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(movie.releaseDate) else ""
+
+        Glide.with(requireContext())
+            .load(movie.posterPath)
+            .error(R.drawable.ic_baseline_image_not_supported_24)
+            .into(poster)
+    }
+
+    override fun showError(throwable: Throwable) {
+        Toast.makeText(context, getString(R.string.error), Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/applicationarchitecturegeekbrains/presentation/movie/MovieDetailsView.kt
+++ app/src/main/java/com/example/applicationarchitecturegeekbrains/presentation/movie/MovieDetailsView.kt
@@ -1,6 +1,16 @@
 package com.example.applicationarchitecturegeekbrains.presentation.movie
 
+import com.example.applicationarchitecturegeekbrains.presentation.model.MovieModel
 import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
 
 interface MovieDetailsView : MvpView {
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showMovie(movie: MovieModel)
+
+    @StateStrategyType(AddToEndSingleStrategy::class)
+    fun showError(throwable: Throwable)
+
 }
